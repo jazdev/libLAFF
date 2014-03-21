c5632e793a21186bbcbfa07c4b7af99500d0bb0c
Jasdev Singh <jasdev@live.in>
2014-03-21 09:55:07 -0700

    added scal module

--- laff/scal.py
+++ laff/scal.py
@@ -4,4 +4,15 @@
 from numpy import *
 
 def scal(alpha, x):
-    pass
+    """
+    Compute alpha * x, overwriting x
+    
+    x can be row or column vectors.
+    """
+    m_x, n_x = x.shape
+
+    if m_x is 1: # x is a row
+        for i in range(n_x): x[0, i] = alpha * x[0, i]
+            
+    elif n_x is 1: # x is a column
+        for i in range(m_x): x[i, 0] = alpha * x[i, 0]
