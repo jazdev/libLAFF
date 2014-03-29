9818f398bcf2efad22c7756bd114e9dd834c3e74
Jasdev Singh <jasdev@live.in>
2014-03-29 09:41:19 -0700

    cleaned Set_to_zero

--- Set_to_zero.py
+++ Set_to_zero.py
@@ -4,24 +4,21 @@ import flame
 import laff as laff
 
 def Set_to_zero_unb_var1(A):
+    """
+	Set_to_zero_unb_var1(matrix)	
+	
+	Sets the input matxix to zeros
+    """
 
-    AL, AR = flame.part_1x2(A, \
-                            0, 'LEFT')
+    AL, AR = flame.part_1x2(A, 0, 'LEFT')
 
     while AL.shape[1] < A.shape[1]:
 
-        A0, a1, A2 = flame.repart_1x2_to_1x3(AL, AR, \
-                                             1, 'RIGHT')
-
-        #------------------------------------------------------------#
+        A0, a1, A2 = flame.repart_1x2_to_1x3(AL, AR, 1, 'RIGHT')
 
         laff.zerov(a1)
-        
 
-        #------------------------------------------------------------#
-
-        AL, AR = flame.cont_with_1x3_to_1x2(A0, a1, A2, \
-                                            'LEFT')
+        AL, AR = flame.cont_with_1x3_to_1x2(A0, a1, A2, 'LEFT')
 
     flame.merge_1x2(AL, AR, A)
 
