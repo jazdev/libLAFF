228d3de80d9a60067e866129d36f8edf1717e88f
Jasdev Singh <jasdev@live.in>
2014-04-02 13:06:03 -0700

    updated O and I

--- Set_to_identity.py
+++ Set_to_identity.py
@@ -5,7 +5,14 @@ import flame
 import laff as laff
 
 def Set_to_identity_unb_var1(A):
-
+    """
+	Set_to_identity_unb_var1(matrix)	
+	
+	Sets the input matrix to the Identity matrix.
+	Traverses input matrix from TOP-LEFT to BOTTOM-RIGHT
+	and sets results column-wise.
+    """
+    
     ATL, ATR, \
     ABL, ABR  = flame.part_2x2(A, \
                                0, 0, 'TL')
@@ -31,3 +38,37 @@ def Set_to_identity_unb_var1(A):
     flame.merge_2x2(ATL, ATR, \
                     ABL, ABR, A)
 
+
+def Set_to_identity_unb_var2(A):
+    """
+	Set_to_identity_unb_var2(matrix)	
+	
+	Sets the input matrix to the Identity matrix.
+	Traverses input matrix from TOP-LEFT to BOTTOM-RIGHT
+	and dets results row-wise.
+    """
+    
+    ATL, ATR, \
+    ABL, ABR  = flame.part_2x2(A, \
+                               0, 0, 'TL')
+
+    while ATL.shape[0] < A.shape[0]:
+
+        A00,  a01,     A02,  \
+        a10t, alpha11, a12t, \
+        A20,  a21,     A22   = flame.repart_2x2_to_3x3(ATL, ATR, \
+                                                       ABL, ABR, \
+                                                       1, 1, 'BR')
+
+        laff.zerov(a10t)
+        laff.onev(alpha11)
+        laff.zerov(a12t)
+
+        ATL, ATR, \
+        ABL, ABR  = flame.cont_with_3x3_to_2x2(A00,  a01,     A02,  \
+                                               a10t, alpha11, a12t, \
+                                               A20,  a21,     A22,  \
+                                               'TL')
+
+    flame.merge_2x2(ATL, ATR, \
+                    ABL, ABR, A)
--- Set_to_zero.py
+++ Set_to_zero.py
@@ -7,18 +7,62 @@ def Set_to_zero_unb_var1(A):
     """
 	Set_to_zero_unb_var1(matrix)	
 	
-	Sets the input matxix to zeros
+	Sets the input matrix to zeros.
+	Traverses input matrix from LEFT to RIGHT
+	and sets results column-wise.
     """
 
-    AL, AR = flame.part_1x2(A, 0, 'LEFT')
+    AL, \
+    AR = flame.part_1x2(A, \
+                        0, 'LEFT')
 
     while AL.shape[1] < A.shape[1]:
 
-        A0, a1, A2 = flame.repart_1x2_to_1x3(AL, AR, 1, 'RIGHT')
+        A0, \
+        a1, \
+        A2 = flame.repart_1x2_to_1x3(AL, \
+                                     AR, \
+                                     1, 'RIGHT')
 
         laff.zerov(a1)
 
-        AL, AR = flame.cont_with_1x3_to_1x2(A0, a1, A2, 'LEFT')
+        AL, \
+        AR = flame.cont_with_1x3_to_1x2(A0, \
+                                        a1, \
+                                        A2, 'LEFT')
 
-    flame.merge_1x2(AL, AR, A)
+    flame.merge_1x2(AL, \
+                    AR, A)
 
+
+def Set_to_zero_unb_var2(A):
+    """
+	Set_to_zero_unb_var2(matrix)	
+	
+	Sets the input matrix to zeros.
+	Traverses input matrix from TOP to BOTTOM
+	and sets results column-wise.
+    """
+
+    AT, \
+    AB  = flame.part_2x1(A, \
+                         0, 'TOP')
+
+    while AT.shape[0] < A.shape[0]:
+
+        A0,  \
+        a1t, \
+        A2   = flame.repart_2x1_to_3x1(AT, \
+                                       AB, \
+                                       1, 'BOTTOM')
+
+        laff.zerov(a1t)
+
+        AT, \
+        AB  = flame.cont_with_3x1_to_2x1(A0,  \
+                                         a1t, \
+                                         A2,  \
+                                         'TOP')
+
+    flame.merge_2x1(AT, \
+                    AB, A)
