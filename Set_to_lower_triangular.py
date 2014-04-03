6399a362be3332a581fb67e38860cc39f8cca603
Jasdev Singh <jasdev@live.in>
2014-04-03 12:42:57 -0700

    added Set_to_lower_triangular

---
+++ Set_to_lower_triangular.py
@@ -0,0 +1,74 @@
+# Programmed by: jasdev
+#                
+
+import flame
+import laff as laff
+
+def Set_to_lower_triangular_matrix_unb_var1(A):
+    """
+	Set_to_lower_triangular_matrix_unb_var1(matrix)	
+	
+	Sets the above diagonal elements of A to zero.
+
+	Traverses matrix A from TOP-LEFT to BOTTOM-RIGHT,
+	and sets results column-wise.
+    """
+    
+    ATL, ATR, \
+    ABL, ABR  = flame.part_2x2(A, \
+                               0, 0, 'TL')
+
+    while ATL.shape[0] < A.shape[0]:
+
+        A00,  a01,     A02,  \
+        a10t, alpha11, a12t, \
+        A20,  a21,     A22   = flame.repart_2x2_to_3x3(ATL, ATR, \
+                                                       ABL, ABR, \
+                                                       1, 1, 'BR')
+
+        laff.zerov(a01)
+
+        ATL, ATR, \
+        ABL, ABR  = flame.cont_with_3x3_to_2x2(A00,  a01,     A02,  \
+                                               a10t, alpha11, a12t, \
+                                               A20,  a21,     A22,  \
+                                               'TL')
+
+    flame.merge_2x2(ATL, ATR, \
+                    ABL, ABR, A)
+
+
+
+def Set_to_lower_triangular_matrix_unb_var2(A):
+    """
+	Set_to_lower_triangular_matrix_unb_var2(matrix)	
+	
+	Sets the above diagonal elements of A to zero.
+
+	Traverses matrix A from TOP-LEFT to BOTTOM-RIGHT,
+	and sets results row-wise.
+    """
+    
+    ATL, ATR, \
+    ABL, ABR  = flame.part_2x2(A, \
+                               0, 0, 'TL')
+
+    while ATL.shape[0] < A.shape[0]:
+
+        A00,  a01,     A02,  \
+        a10t, alpha11, a12t, \
+        A20,  a21,     A22   = flame.repart_2x2_to_3x3(ATL, ATR, \
+                                                       ABL, ABR, \
+                                                       1, 1, 'BR')
+
+        laff.zerov(a12t)
+
+        ATL, ATR, \
+        ABL, ABR  = flame.cont_with_3x3_to_2x2(A00,  a01,     A02,  \
+                                               a10t, alpha11, a12t, \
+                                               A20,  a21,     A22,  \
+                                               'TL')
+
+    flame.merge_2x2(ATL, ATR, \
+                    ABL, ABR, A)
+
--- dummy_tests.py
+++ dummy_tests.py
@@ -1,6 +1,7 @@
 
-from Set_to_zero import Set_to_zero_unb_var1
-from Set_to_identity import Set_to_identity_unb_var1
+from Set_to_zero import Set_to_zero_unb_var1, Set_to_zero_unb_var2
+from Set_to_identity import Set_to_identity_unb_var1, Set_to_identity_unb_var2
+from Set_to_lower_triangular import Set_to_lower_triangular_matrix_unb_var1, Set_to_lower_triangular_matrix_unb_var2
 
 from numpy import random
 from numpy import matrix
@@ -20,17 +21,39 @@ from numpy import matrix
 ###########################################
 
 
-#########################################
-#       Set_to_identity
-#########################################
-A = matrix( random.rand( 5,5 ) )
+###########################################
+###       Set_to_identity
+###########################################
+##A = matrix( random.rand( 5,5 ) )
+##
+##print( 'A before =' )
+##print( A )
+##
+##Set_to_identity_unb_var1( A )
+##
+##print( 'A after =' )
+##print( A )
+###########################################
 
-print( 'A before =' )
-print( A )
 
-Set_to_identity_unb_var1( A )
+###########################################
+###       Set_to_lower_triangular
+###########################################
+##print "Using Set_to_lower_triangular_matrix_unb_var1"
+##A = matrix( random.rand( 5,5 ) )
+##print( 'A before =' )
+##print( A )
+##Set_to_lower_triangular_matrix_unb_var1( A )
+##print( 'A after =' )
+##print( A )
+##
+##print "Using Set_to_lower_triangular_matrix_unb_var2"
+##A = matrix( random.rand( 5,5 ) )
+##print( 'A before =' )
+##print( A )
+##Set_to_lower_triangular_matrix_unb_var2( A )
+##print( 'A after =' )
+##print( A )
+###########################################
 
-print( 'A after =' )
-print( A )
-#########################################
 
