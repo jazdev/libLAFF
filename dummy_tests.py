6052087274d54e581cb6e1fb394d782529ad71ea
Jasdev Singh <jasdev@live.in>
2014-04-03 12:49:48 -0700

    updated Set_to_diagonal

--- dummy_tests.py
+++ dummy_tests.py
@@ -1,6 +1,7 @@
 
 from Set_to_zero import Set_to_zero_unb_var1, Set_to_zero_unb_var2
 from Set_to_identity import Set_to_identity_unb_var1, Set_to_identity_unb_var2
+from Set_to_diagonal import Set_to_diagonal_matrix_unb_var1, Set_to_diagonal_matrix_unb_var2
 from Set_to_lower_triangular import Set_to_lower_triangular_matrix_unb_var1, Set_to_lower_triangular_matrix_unb_var2
 
 from numpy import random
@@ -9,13 +10,19 @@ from numpy import matrix
 ###########################################
 ###       Set_to_zero
 ###########################################
+##print "Using Set_to_zero_unb_var1"
 ##A = matrix( random.rand( 5,4 ) )
-##
 ##print( 'A before =' )
 ##print( A )
-##
 ##Set_to_zero_unb_var1( A )
+##print( 'A after =' )
+##print( A )
 ##
+##print "Using Set_to_zero_unb_var2"
+##A = matrix( random.rand( 5,4 ) )
+##print( 'A before =' )
+##print( A )
+##Set_to_zero_unb_var2( A )
 ##print( 'A after =' )
 ##print( A )
 ###########################################
@@ -24,13 +31,46 @@ from numpy import matrix
 ###########################################
 ###       Set_to_identity
 ###########################################
+##print "Using Set_to_identity_unb_var1"
 ##A = matrix( random.rand( 5,5 ) )
-##
 ##print( 'A before =' )
 ##print( A )
-##
 ##Set_to_identity_unb_var1( A )
+##print( 'A after =' )
+##print( A )
 ##
+##print "Using Set_to_identity_unb_var2"
+##A = matrix( random.rand( 5,5 ) )
+##print( 'A before =' )
+##print( A )
+##Set_to_identity_unb_var2( A )
+##print( 'A after =' )
+##print( A )
+###########################################
+
+
+###########################################
+###       Set_to_diagonal
+###########################################
+##print "Using Set_to_diagonal_matrix_unb_var1"
+##A = matrix( random.rand( 5,5 ) )
+##d = matrix( random.rand( 5,1 ) )
+##print( 'A before =' )
+##print( A )
+##print( 'd before =' )
+##print( d )
+##Set_to_diagonal_matrix_unb_var1( d, A )
+##print( 'A after =' )
+##print( A )
+##
+##print "Using Set_to_diagonal_matrix_unb_var2"
+##A = matrix( random.rand( 5,5 ) )
+##d = matrix( random.rand( 5,1 ) )
+##print( 'A before =' )
+##print( A )
+##print( 'd before =' )
+##print( d )
+##Set_to_diagonal_matrix_unb_var2( d, A )
 ##print( 'A after =' )
 ##print( A )
 ###########################################
